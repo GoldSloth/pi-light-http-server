c3240e8fecef7a01bfc816ab67bcef6942209a28
GolddolphinSKB-Fexcraftian <games.tudorrose@gmail.com>
2019-01-01 19:18:29 +0000

    Updated front end

--- front-end.js
+++ front-end.js
@@ -1,5 +1,56 @@
+function rgbToHex(r, g, b) {
+    return "#" + Math.floor(r * 255).toString(16) + Math.floor(g * 255).toString(16) + Math.floor(b * 255).toString(16)
+}
+
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+var colourPicker = document.getElementById("colourPicker")
+var brightness = document.getElementById("brightness")
+var consoleOutput = document.getElementById("consoleOutput")
+
+var status
+
 function updatePage() {
-    console.log(this.responseText)
+    resp = this.responseText.replace(/'/g, "\"")
+    consoleOutput.innerHTML += "Recieved packet: " + resp
+    console.log(resp)
+    var status = JSON.parse(resp)
+
+    colourPicker.value = rgbToHex(status.colour.red, status.colour.green, status.colour.blue)
+    brightness.value = status.brightness
+}
+
+function sendPacket() {
+    var req = new XMLHttpRequest()
+
+    req.addEventListener("load", response)
+    req.open("POST", "http://192.168.1.75:8000")
+    req.setRequestHeader("Access-Control-Allow-Origin", "*")
+    req.send("SET"+JSON.stringify(status))
+}
+
+function updateLog() {
+    consoleOutput.innerHTML += "Recieved" + this.responseText
+}
+
+function changeStatus() {
+    var newColour = hexToRgb(colourPicker.value) 
+    status = {
+        "colour": 
+        {
+            "red": newColour.r / 255,
+            "green": newColour.g / 255,
+            "blue": newColour.b / 255
+        },
+        "brightness": brightness.value
+    }
 }
 
 var request = new XMLHttpRequest()
@@ -8,3 +59,6 @@ request.addEventListener("load", updatePage)
 request.open("GET", "http://192.168.1.75:8000")
 request.setRequestHeader("Access-Control-Allow-Origin", "*")
 request.send()
+
+colourPicker.addEventListener("onChange", changeStatus)
+brightness.addEventListener("onChange", changeStatus)
