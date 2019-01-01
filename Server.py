9ee0086704bdd41ef2fc041c2bb5ae3d08868ec2
GolddolphinSKB-Fexcraftian <games.tudorrose@gmail.com>
2019-01-01 16:54:04 +0000

    Fixed global syntax, again

--- Server.py
+++ Server.py
@@ -2,6 +2,9 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from LightHandler import LightStrip
 
 class Server(BaseHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super(Server, self).__init__(*args, **kwargs)
+        global lights
     def _set_headers(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
