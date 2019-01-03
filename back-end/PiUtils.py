d6fff00db5e9e956736f3b44a2bef430c093f654
GolddolphinSKB-Fexcraftian <games.tudorrose@gmail.com>
2019-01-03 21:24:02 +0000

    String slice wrong way around :(

--- back-end/PiUtils.py
+++ back-end/PiUtils.py
@@ -8,8 +8,8 @@ def getTemp():
 
 def getRAM():
     result = os.popen('free').readlines()
-    rmem = str(result[1])[:5].split(" ")
-    rswap = str(result[2])[:7].split(" ")
+    rmem = str(result[1])[5:].split(" ")
+    rswap = str(result[2])[7:].split(" ")
     return {"mem": rmem, "swap": rswap}
 
 
