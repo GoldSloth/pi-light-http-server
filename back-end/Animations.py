8812dffdf9c82e6e80ad0db88a4161ed88ee60ed
GolddolphinSKB-Fexcraftian <games.tudorrose@gmail.com>
2019-01-23 17:17:47 +0000

    Added print to animation program

--- back-end/Animations.py
+++ back-end/Animations.py
@@ -4,7 +4,9 @@ import colorsys
 def progressiveSinWave(x, t):
     d = (math.sin((t + x) / 1000) + 1) / 2
     col = colorsys.hsv_to_rgb(d, 1, 1)
-    return {"red": col[0], "green": col[1], "blue": col[2]}
+    colObj = {"red": col[0], "green": col[1], "blue": col[2]}
+    print(colObj)
+    return colObj
 
 def totalSinWave(x, t):
     d = (math.sin(t / 1000) + 1) / 2
